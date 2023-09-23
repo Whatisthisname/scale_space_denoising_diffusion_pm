a62d553f46ecf096218adbdf66083715a93b9eed
Theo <theoruterwurtzen@gmail.com>
2023-09-23 15:27:46 +0200

    reduced images to 2 and 5

--- unet.py
+++ unet.py
@@ -1,136 +1,174 @@
 import torch
-import torch.functional as F
 import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
 
-# define our U-net model
-class UNet(nn.Module):
-    # images have 1 channel (grayscale).
-    # `n_downs` 
 
-    def __init__(self, image_size : int, stages : int, context_size : int = 1):
-        """`stages` defines the number of downsampling and upsampling stages. 0 stages means no downsampling or upsampling."""
-        super(UNet, self).__init__()
-        self.image_size = image_size
-        self.stages = stages
+class ChannelShuffle(nn.Module):
+    def __init__(self,groups):
+        super().__init__()
+        self.groups=groups
+    def forward(self,x):
+        n,c,h,w=x.shape
+        x=x.view(n,self.groups,c//self.groups,h,w) # group
+        x=x.transpose(1,2).contiguous().view(n,-1,h,w) #shuffle
         
-        channel_inflation = 16
+        return x
 
-        self.context_size = context_size
-        self.encoders = nn.ModuleList()
-        self.decoders = nn.ModuleList()
+class ConvBnSiLu(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0):
+        super().__init__()
+        self.module=nn.Sequential(nn.Conv2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding),
+                                  nn.BatchNorm2d(out_channels),
+                                  nn.SiLU(inplace=True))
+    def forward(self,x):
+        return self.module(x)
 
-        self.encoders.append(EncoderBlock(1, channel_inflation, downsample=False))
-        for i in range(stages):
-                self.encoders.append(EncoderBlock(channel_inflation * (2 ** (i)), channel_inflation * (2 ** (i+1))))
-        
-        if stages == 0:
-            self.decoders.append(DecoderBlock(channel_inflation,  channel_inflation, upsample=False))
+class ResidualBottleneck(nn.Module):
+    '''
+    shufflenet_v2 basic unit(https://arxiv.org/pdf/1807.11164.pdf)
+    '''
+    def __init__(self,in_channels,out_channels):
+        super().__init__()
 
-        else:
-            self.decoders.append(DecoderBlock(channel_inflation * 2 **(stages),  channel_inflation * 2 **(stages-1), upsample=True))
-            for i in range(stages-1, 0, -1):
-                self.decoders.append(DecoderBlock(2 * channel_inflation * (2 ** (i)), channel_inflation * (2 ** (i-1)), upsample=True))
+        self.branch1=nn.Sequential(nn.Conv2d(in_channels//2,in_channels//2,3,1,1,groups=in_channels//2),
+                                    nn.BatchNorm2d(in_channels//2),
+                                    ConvBnSiLu(in_channels//2,out_channels//2,1,1,0))
+        self.branch2=nn.Sequential(ConvBnSiLu(in_channels//2,in_channels//2,1,1,0),
+                                    nn.Conv2d(in_channels//2,in_channels//2,3,1,1,groups=in_channels//2),
+                                    nn.BatchNorm2d(in_channels//2),
+                                    ConvBnSiLu(in_channels//2,out_channels//2,1,1,0))
+        self.channel_shuffle=ChannelShuffle(2)
 
-            self.decoders.append(DecoderBlock(2*channel_inflation,  channel_inflation * 1, upsample=False))
-        
-        self.final_conv = nn.Conv2d(channel_inflation, 1, kernel_size=3, padding=1)
-
-        # print ("Conv2D: in_channels: {}, out_channels: {}".format(channel_inflation, 1))
-
-
-    def forward(self, x : torch.Tensor) -> torch.Tensor:
-        # define the forward pass using skip connections
-        
-        # encoder
-        intermediate_encodings = []
-        for i in range(self.stages+1):
-            x = self.encoders[i](x)                
-            intermediate_encodings.append(x)
-            # print ("after encoder", i, x.shape)
-        intermediate_encodings.pop() # we don't need to concatenate the last layer as it goes directly to the decoder
-
-        intermediate_encodings.reverse() 
-        
-        # decoder
-        for i in range(self.stages+1):
-
-            if i > 0:
-                # concatenate the previous conv in the encoding stage to feed to the decoding (skip connection)
-                x = torch.cat((x, intermediate_encodings[i-1]), dim=1)
-            x = self.decoders[i](x)
-
-            
-        x = self.final_conv(x)
+    def forward(self,x):
+        x1,x2=x.chunk(2,dim=1)
+        x=torch.cat([self.branch1(x1),self.branch2(x2)],dim=1)
+        x=self.channel_shuffle(x) #shuffle two branches
 
         return x
 
+class ResidualDownsample(nn.Module):
+    '''
+    shufflenet_v2 unit for spatial down sampling(https://arxiv.org/pdf/1807.11164.pdf)
+    '''
+    def __init__(self,in_channels,out_channels):
+        super().__init__()
+        self.branch1=nn.Sequential(nn.Conv2d(in_channels,in_channels,3,2,1,groups=in_channels),
+                                    nn.BatchNorm2d(in_channels),
+                                    ConvBnSiLu(in_channels,out_channels//2,1,1,0))
+        self.branch2=nn.Sequential(ConvBnSiLu(in_channels,out_channels//2,1,1,0),
+                                    nn.Conv2d(out_channels//2,out_channels//2,3,2,1,groups=out_channels//2),
+                                    nn.BatchNorm2d(out_channels//2),
+                                    ConvBnSiLu(out_channels//2,out_channels//2,1,1,0))
+        self.channel_shuffle=ChannelShuffle(2)
+
+    def forward(self,x):
+        x=torch.cat([self.branch1(x),self.branch2(x)],dim=1)
+        x=self.channel_shuffle(x) #shuffle two branches
+
+        return x
+
+class TimeMLP(nn.Module):
+    '''
+    naive introduce timestep information to feature maps with mlp and add shortcut
+    '''
+    def __init__(self,embedding_dim,hidden_dim,out_dim):
+        super().__init__()
+        self.mlp=nn.Sequential(nn.Linear(embedding_dim,hidden_dim),
+                                nn.SiLU(),
+                               nn.Linear(hidden_dim,out_dim))
+        self.act=nn.SiLU()
+    def forward(self,x,t):
+        t_emb=self.mlp(t).unsqueeze(-1).unsqueeze(-1)
+        x=x+t_emb
+  
+        return self.act(x)
+    
 class EncoderBlock(nn.Module):
-    # takes input size and output size
-    def __init__(self, in_channels : int, out_channels : int, context_size : int, downsample : bool = True):
-        super(EncoderBlock, self).__init__()
-        in_channels = int(in_channels)
-        out_channels = int(out_channels)
-        self.downsample = downsample
-        self.context_size = context_size
+    def __init__(self,in_channels,out_channels,time_embedding_dim):
+        super().__init__()
+        self.conv0=nn.Sequential(*[ResidualBottleneck(in_channels,in_channels) for i in range(3)],
+                                    ResidualBottleneck(in_channels,out_channels//2))
 
-        # log input params:
-        # print ("EncoderBlock: in_channels: {}, out_channels: {}".format(in_channels, out_channels), end = " ")
-        # print ("downsample immediately: {}".format(downsample))
+        self.time_mlp=TimeMLP(embedding_dim=time_embedding_dim,hidden_dim=out_channels,out_dim=out_channels//2)
+        self.conv1=ResidualDownsample(out_channels//2,out_channels)
+    
+    def forward(self,x,t=None):
+        x_shortcut=self.conv0(x)
+        if t is not None:
+            x=self.time_mlp(x_shortcut,t)
+        x=self.conv1(x)
 
-        # define the layers of the encoder block
+        return [x,x_shortcut]
         
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        if downsample:
-            self.pool = nn.MaxPool2d(2, 2)
-        self.gelu = nn.GELU()
-        self.batchnorm = nn.BatchNorm2d(out_channels)
-
-    def forward(self, x : torch.Tensor) -> torch.Tensor:
-        if self.downsample:
-            x = self.pool(x)
-        x = self.conv1(x)
-        # print ("encoder conv1 shape: {}".format(x.shape))
-        x = self.gelu(x)
-        x = self.conv2(x)
-        # print ("encoder conv2 shape: {}".format(x.shape))
-        x = self.gelu(x)
-        # x = self.batchnorm(x)
-        # print ("encoder pool shape: {}".format(x.shape))
-        return x
-
 class DecoderBlock(nn.Module):
-    # takes input size and output size
-    def __init__(self, in_channels : int, out_channels : int, context_size : int, upsample : bool = True):
-        super(DecoderBlock, self).__init__()
-        in_channels = int(in_channels)
-        out_channels = int(out_channels)
-        self.upsample = upsample
-        
-        # log input params:
-        # print ("DecoderBlock: in_channels: {}, out_channels: {}".format(in_channels, out_channels), end = " ")
-        # print ("upsample at end: {}".format(upsample))
+    def __init__(self,in_channels,out_channels,time_embedding_dim):
+        super().__init__()
+        self.upsample=nn.Upsample(scale_factor=2,mode='bilinear',align_corners=False)
+        self.conv0=nn.Sequential(*[ResidualBottleneck(in_channels,in_channels) for i in range(3)],
+                                    ResidualBottleneck(in_channels,in_channels//2))
 
-        # define the layers of the decoder block
-        if upsample:
-            self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.gelu = nn.GELU()
-        # self.batchnorm = nn.BatchNorm2d(out_channels)
+        self.time_mlp=TimeMLP(embedding_dim=time_embedding_dim,hidden_dim=in_channels,out_dim=in_channels//2)
+        self.conv1=ResidualBottleneck(in_channels//2,out_channels//2)
 
-    def forward(self, x : torch.Tensor) -> torch.Tensor:
-        # print ("decoder befupsampled shape: {}".format(x.shape))
-        # print ("decoder upsampled shape: {}".format(x.shape))
-        x = self.conv1(x)
-        # print ("decoder conv1 shape: {}".format(x.shape))
-        x = self.gelu(x)
-        x = self.conv2(x)
-        # print ("decoder conv2 shape: {}".format(x.shape))
-        x = self.gelu(x)
-        # x = self.batchnorm(x)
-        if self.upsample:
-            x = self.upsample(x)
+    def forward(self,x,x_shortcut,t=None):
+        x=self.upsample(x)
+        x=torch.cat([x,x_shortcut],dim=1)
+        x=self.conv0(x)
+        if t is not None:
+            x=self.time_mlp(x,t)
+        x=self.conv1(x)
+
+        return x        
+
+class Unet(nn.Module):
+    '''
+    simple unet design without attention
+    '''
+    def __init__(self,timesteps,time_embedding_dim,in_channels=3,out_channels=2,base_dim=32,dim_mults=[2,4,8,16]):
+        super().__init__()
+        assert isinstance(dim_mults,(list,tuple))
+        assert base_dim%2==0 
+
+        channels=self._cal_channels(base_dim,dim_mults)
+
+        self.init_conv=ConvBnSiLu(in_channels,base_dim,3,1,1)
+        self.time_embedding=nn.Embedding(timesteps,time_embedding_dim)
+
+        self.encoder_blocks=nn.ModuleList([EncoderBlock(c[0],c[1],time_embedding_dim) for c in channels])
+        self.decoder_blocks=nn.ModuleList([DecoderBlock(c[1],c[0],time_embedding_dim) for c in channels[::-1]])
+    
+        self.mid_block=nn.Sequential(*[ResidualBottleneck(channels[-1][1],channels[-1][1]) for i in range(2)],
+                                        ResidualBottleneck(channels[-1][1],channels[-1][1]//2))
+
+        self.final_conv=nn.Conv2d(in_channels=channels[0][0]//2,out_channels=out_channels,kernel_size=1)
+
+    def forward(self,x,t=None):
+        x=self.init_conv(x)
+        if t is not None:
+            t=self.time_embedding(t)
+        encoder_shortcuts=[]
+        for encoder_block in self.encoder_blocks:
+            x,x_shortcut=encoder_block(x,t)
+            encoder_shortcuts.append(x_shortcut)
+        x=self.mid_block(x)
+        encoder_shortcuts.reverse()
+        for decoder_block,shortcut in zip(self.decoder_blocks,encoder_shortcuts):
+            x=decoder_block(x,shortcut,t)
+        x=self.final_conv(x)
+
         return x
+
+    def _cal_channels(self,base_dim,dim_mults):
+        dims=[base_dim*x for x in dim_mults]
+        dims.insert(0,base_dim)
+        channels=[]
+        for i in range(len(dims)-1):
+            channels.append((dims[i],dims[i+1])) # in_channel, out_channel
+
+        return channels
+
+if __name__=="__main__":
+    x=torch.randn(3,3,224,224)
+    t=torch.randint(0,1000,(3,))
+    model=Unet(1000,128)
+    y=model(x,t)
+    print(y.shape)
